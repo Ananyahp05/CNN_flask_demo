9efca6ad4a41f1a9a6caaf4b3ce6491ef768f869
Ananyahp05 <ananyahp580@gmail.com>
2026-01-31 14:31:42 +0530

    CNN flask

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/<name>")
+def name(name):
+    # name=name.capitalize()
+    return render_template("name.html",name=name,image=name+".png")
+
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
+
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=5000,use_reloader=False) 
